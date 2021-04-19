57164a04b2cbdf906aeb2b088d22588b23f91daf
52614 <52614@LAPTOP-ACCAD5FF>
2021-04-19 12:43:51 -0600

    Programa despliega saludos, primer ejercicio

---
+++ src/ejer_github/Ejer_Github.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejer_github;
+
+/**
+ *
+ * @author 52614
+ */
+public class Ejer_Github {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hola gurpo de POO");
+    }
+    
+}
